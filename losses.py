a36f8f6a18da7c3d222b85f465aa2932d654b23b
Tomo Oga <oga.t@northeastern.edu>
2026-03-12 14:16:04 -0400

    change red from sum to mean

--- losses.py
+++ losses.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn.functional as F
 
 
-def vae_tabular_loss(x, x_recon, mu, logvar, ncont=30, beta=1.0):
+def vae_tabular_loss(x, x_recon, mu, logvar, ncont=30, beta=0.01):
     x_cont = x[:, :ncont]
     x_recon_cont = x_recon[:, :ncont]
-    recon_loss_cont = F.mse_loss(x_recon_cont, x_cont, reduction='sum')
+    recon_loss_cont = F.mse_loss(x_recon_cont, x_cont)
 
     y_true = x[:, ncont:].unsqueeze(1)
     y_pred = x_recon[:, ncont:].unsqueeze(1)
-    recon_loss_cat = F.binary_cross_entropy_with_logits(y_pred, y_true, reduction='sum')
+    recon_loss_cat = F.binary_cross_entropy_with_logits(y_pred, y_true)
 
     kl = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
